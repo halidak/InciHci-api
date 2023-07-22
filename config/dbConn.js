b6bcffbca4f30d610f5fc65edf0d896e317c72a5
halidak <halida.karisik6@gmail.com>
2023-07-22 18:59:01 +0200

    config

---
+++ config/dbConn.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`Connected to MongoDB: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Error connecting to MongoDB: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
+
+
